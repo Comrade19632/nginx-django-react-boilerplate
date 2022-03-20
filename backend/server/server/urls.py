ae8cf6d5590fc80e1dd43ae686fc28d522a40fae
Artem Kasheev <comrade19632@gmail.com>
2022-03-21 02:25:03 +0300

    fix media and static serving

--- backend/server/server/urls.py
+++ backend/server/server/urls.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 from django.urls import path
+from django.conf import settings
+from django.conf.urls.static import static
 
 from apps.accounts.urls import accounts_urlpatterns
 from apps.notes.urls import notes_urlpatterns
@@ -7,7 +9,7 @@ from apps.notes.urls import notes_urlpatterns
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 urlpatterns += accounts_urlpatterns  # add URLs for authentication
 urlpatterns += notes_urlpatterns  # notes URLs
