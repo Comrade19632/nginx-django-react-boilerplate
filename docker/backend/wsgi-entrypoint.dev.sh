621c3fd143abe7f6625564584bf278112b6f2c83
Artem Kasheev <comrade19632@gmail.com>
2022-03-21 13:43:30 +0300

    return collectstatic to dev

--- docker/backend/wsgi-entrypoint.dev.sh
+++ docker/backend/wsgi-entrypoint.dev.sh
@@ -11,4 +11,6 @@ do
     sleep 2
 done
 
+./manage.py collectstatic --noinput
+
 watchmedo auto-restart --recursive -d . -p '*.py' -- gunicorn server.wsgi --bind 0.0.0.0:8000 --workers 4 --threads 4
